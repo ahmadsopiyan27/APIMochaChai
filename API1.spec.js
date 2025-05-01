b721e0a7b11b336367e4fc60800d28e0c34bdcb2
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-05-01 14:50:47 +0700

    Belajar

---
+++ API1.spec.js
@@ -0,0 +1,13 @@
+let statusCode = 200;
+
+
+describe('hit API login', function () {
+    context('login success', function () {
+        it('statusCode 200', function () {
+            console.log(200);
+            
+        })
+        
+    })
+    
+})
