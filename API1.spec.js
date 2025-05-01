30cd9a551f30763cb4d8a7732825425426172cfb
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-05-01 20:02:40 +0700

    update failed status code 404

--- API1.spec.js
+++ API1.spec.js
@@ -1,13 +1,17 @@
 let statusCode = 200;
+let failedstatusCode = 404;
 
 
 describe('hit API login', function () {
     context('login success', function () {
         it('statusCode 200', function () {
-            console.log(200);
+            console.log(statusCode);
             
         })
-        
+        it ('statusCode 404', function () {
+            console.log(failedstatusCode)
+            
+        })
     })
     
 })
