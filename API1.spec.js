3175381fb32baa0f0e53d0f4df0108aa9deb213a
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-05-01 22:48:05 +0700

    Error 404

--- API1.spec.js
+++ API1.spec.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
+// const assert = chai.should;
 const request = require('supertest')
 
 
@@ -10,10 +11,17 @@ let failedstatusCode = 404;
 let methode = 'get';
 let url = 'https://fakestoreapi.com';
 let endpoint = '/products'
-
+let body = {
+    "id": 0,
+    "title": "string",
+    "price": 0.1,
+    "description": "string",
+    "category": "string",
+    "image": "http://example.com"
+  }
 
 describe('success', function () {
-    context('hit api login', function () {
+    context('hit api get products', function () {
         it('statusCode 200', async function () {
             const response = await request(url).get(endpoint);
         
@@ -25,6 +33,33 @@ describe('success', function () {
             const response = await request(url).get(`${endpoint}s`)
           expect(response.statusCode).to.eq(404);
         });
+    })
+    context('hit api create products', function () {
+        it('success', async function () {
+            const response = await request(url).post(endpoint).send(body);
+            expect(response.statusCode).to.eq(200);
+            expect(response.body.title).to.eq(body.title);
+            
+        });
+
+
+        it('gagal', async function () {
+            // let wrongBody = {... body};
+            // wrongBody.title = 123123;
+            let wrongBody = {
+                id: 21,
+                title: 123123,
+                price: 0.1,
+                description: 'string',
+                image: 'http://example.com',
+                category: 'string'
+              }
+
+            const response = await request(url).post(endpoint).send(wrongBody);
+            console.log(response.body);
+            expect(response.statusCode).to.eq(400);
+            
+        })
     })
     
 });
