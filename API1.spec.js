95023af4a39b72049abc49b75f5623ce58b3514f
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-05-01 21:04:43 +0700

    update version NPM

--- API1.spec.js
+++ API1.spec.js
@@ -1,17 +1,24 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+
+
 let statusCode = 200;
 let failedstatusCode = 404;
 
 
-describe('hit API login', function () {
-    context('login success', function () {
+describe('success', function () {
+    context('hit api login', function () {
         it('statusCode 200', function () {
-            console.log(statusCode);
+            // console.log(statusCode);
+            expect(statusCode).to.eq(200)
+
             
-        })
-        it ('statusCode 404', function () {
-            console.log(failedstatusCode)
-            
-        })
+        });
+        it ('failed', function () {
+            // console.log(failedstatusCode)
+            expect(failedstatusCode).to.eq(404)
+        });
     })
     
-})
+});
