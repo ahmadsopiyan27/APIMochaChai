855fcb719d8e262f4e1ba43e4ff5c7d08b945418
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-05-01 22:16:04 +0700

    assync & await

--- API1.spec.js
+++ API1.spec.js
@@ -1,23 +1,29 @@
 const chai = require('chai');
 const expect = chai.expect;
+const request = require('supertest')
 
 
 
 let statusCode = 200;
 let failedstatusCode = 404;
 
+let methode = 'get';
+let url = 'https://fakestoreapi.com';
+let endpoint = '/products'
+
 
 describe('success', function () {
     context('hit api login', function () {
-        it('statusCode 200', function () {
-            // console.log(statusCode);
-            expect(statusCode).to.eq(200)
+        it('statusCode 200', async function () {
+            const response = await request(url).get(endpoint);
+        
+            expect(response.statusCode).to.eq(200);
+            expect(response.body[0].id).to.eq(1);
 
-            
         });
-        it ('failed', function () {
-            // console.log(failedstatusCode)
-            expect(failedstatusCode).to.eq(404)
+        it ('failed', async function () {
+            const response = await request(url).get(`${endpoint}s`)
+          expect(response.statusCode).to.eq(404);
         });
     })
     
