1cff591f86073645d9fd79436ac908a76f58a438
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-05-03 14:29:59 +0700

    update parameter

--- Header.spec.js
+++ Header.spec.js
@@ -1,19 +1,29 @@
 const request = require('supertest')
 
-describe('coba header', async function () {
+describe('coba header', function () {
+    it('login Apicheap', async function () {
+        
+    
     let header= {
         'x-access-token': '321d6a221f8926b5ec41ae89a3b2ae7b'
     }
     let parameter = {
         'origin' : "MOW",
         'destination': 'HKT',
-        'depart_date' : '2025-04',
-        'return_date' : '2025-05'
+        'depart_date' : '2025-11',
+        'return_date' : '2025-11',
+        'page' : '1',
+        'currency' : 'RUB'
 
     }
+    // let id = [123,124,125]
     let url = 'https://api.travelpayouts.com';
-    let endpoint = '/v1/prices/monthly'
+    let endpoint = '/v1/prices/cheap'
+
+    // let newEndpoint = `${endpoint}/$[0]`
+    // console.log(`${url}${endpoint}`);
 
     const response = await request(url).get(endpoint).set(header).query(parameter);
     console.log(response.body);
-})
+});
+});
